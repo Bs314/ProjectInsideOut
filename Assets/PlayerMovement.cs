1b00262e6b23d6b20aefb6a6ffe570c66d8d6643
Bs314 <104457850+Bs314@users.noreply.github.com>
2024-08-10 00:48:41 +0300

    Platform ready

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -12,43 +12,60 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpinPower = 16f;
     [SerializeField] float fallingPowerFactor = 0.5f;
     private bool isFacingRight = true;
+    public bool isGrounded = true;
+    [SerializeField] Animator anim;
 
-
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform groundCheck;
+    //[SerializeField] private LayerMask groundLayer;
+    //[SerializeField] private Transform groundCheck;
     [SerializeField] private Rigidbody2D body;
     
     void Update()
     {    
-        horizontal = Input.GetAxisRaw("Horizontal");  
-        Flip();  
-        Jump();
-        RotationLimit();
+        
 
     }   
 
     private void FixedUpdate() 
     {
-        body.velocity = new Vector2(horizontal*speed,body.velocity.y);    
+        horizontal = Input.GetAxisRaw("Horizontal");  
+        body.velocity = new Vector2(horizontal*speed,body.velocity.y);   
+        
+        Flip();  
+        Jump();
+        RotationLimit();   
     }
 
     private void Jump()
     {
-        if(Input.GetButtonDown("Jump") && IsGrounded())
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             body.velocity = new Vector2(body.velocity.x , jumpinPower);
         }
 
-        if(Input.GetButtonUp("Jump") && body.velocity.y > 0f)
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground")
         {
-            body.velocity = new Vector2(body.velocity.x , body.velocity.y * fallingPowerFactor);
+            isGrounded = true;
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
         }
     }
 
+    
+    /*
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
+    */
+
     private void Flip()
     {
         if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
